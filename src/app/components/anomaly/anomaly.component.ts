d43b2c0eee70a26f79fb58516018c2a6fe516a0e
Dipta Barua <diptab11@gmail.com>
2021-06-07 17:19:12 +0900

    Anomaly List

--- src/app/components/anomaly/anomaly.component.ts
+++ src/app/components/anomaly/anomaly.component.ts
@@ -1,26 +1,26 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {MatPaginator} from "@angular/material/paginator";
-import {MatSort} from "@angular/material/sort";
-import {QueryParamsModel} from "../../models/query-param.model";
-import {AnomalyService} from "../../services/anomaly.service";
-import {AnomalyModel} from "../../models/anomaly.model";
-import {MatDialog} from "@angular/material/dialog";
-import {MatSnackBar} from "@angular/material/snack-bar";
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { QueryParamsModel } from "../../models/query-param.model";
+import { AnomalyService } from "../../services/anomaly.service";
+import { AnomalyModel } from "../../models/anomaly.model";
+import { MatDialog } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import Swal from 'sweetalert2';
 import { Routes, RouterModule, ActivatedRoute, Router } from '@angular/router';
 
 
 import { AddAnomalyComponent } from '../add-anomaly/add-anomaly.component'
 @Component({
-  selector: 'app-anomaly',
-  templateUrl: './anomaly.component.html',
-  styleUrls: ['./anomaly.component.scss']
+    selector: 'app-anomaly',
+    templateUrl: './anomaly.component.html',
+    styleUrls: ['./anomaly.component.scss']
 })
 export class AnomalyComponent implements OnInit {
 
-    displayedColumns = ['id', 'title', 'anomalyType', 'createdAt', 'event', 'edit', 'delete'];
+    displayedColumns = ['id', 'title', 'anomalyType', 'createdAt', 'map', 'reports', 'edit', 'delete'];
     dataSource = []
-    searchText=''
+    searchText = ''
     pageSize = 100;
 
     currentPage = 0;
@@ -35,12 +35,12 @@ export class AnomalyComponent implements OnInit {
     @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
     @ViewChild(MatSort, { static: true }) sort: MatSort;
     constructor(
-        private  anomalyService: AnomalyService,
+        private anomalyService: AnomalyService,
         public dialog: MatDialog,
         private _snackBar: MatSnackBar,
         private router: Router,
         private route: ActivatedRoute
-    ) {}
+    ) { }
 
     ngOnInit() {
 
@@ -55,31 +55,31 @@ export class AnomalyComponent implements OnInit {
         // this.getAllAnomalys();
     }
 
-        getAllAnomalys(){
-        this.anomalyService.getAllAnomalys().subscribe((res)=>{            
+    getAllAnomalys() {
+        this.anomalyService.getAllAnomalys().subscribe((res) => {
             this.dataSource = res['response'];
             this.totalSize = res['totalCount'];
             this.loading = false;
         });
     }
 
-//     getAllAnomalys(){
-//       this.loading = true;
-//         const queryParams = new QueryParamsModel(
-//             this.filterConfiguration(),
-// //             this.sort.direction,
-//             'desc',
-// //             this.sort.active,
-//             'createdAt',
-//             this.currentPage,
-//             this.pageSize
-//         );
-//         this.anomalyService.getAnomalys(queryParams).subscribe((res)=>{
-//             this.loading = false;
-//             this.dataSource = res['response'];
-//             this.totalSize = res['totalCount'];
-//         });
-//     }
+    //     getAllAnomalys(){
+    //       this.loading = true;
+    //         const queryParams = new QueryParamsModel(
+    //             this.filterConfiguration(),
+    // //             this.sort.direction,
+    //             'desc',
+    // //             this.sort.active,
+    //             'createdAt',
+    //             this.currentPage,
+    //             this.pageSize
+    //         );
+    //         this.anomalyService.getAnomalys(queryParams).subscribe((res)=>{
+    //             this.loading = false;
+    //             this.dataSource = res['response'];
+    //             this.totalSize = res['totalCount'];
+    //         });
+    //     }
 
     filterConfiguration(): any {
         const filter: any = {};
@@ -88,7 +88,7 @@ export class AnomalyComponent implements OnInit {
         return filter;
     }
 
-    deleteAnomaly(row){
+    deleteAnomaly(row) {
         Swal.fire({
             title: '削除しますか？',
             icon: 'warning',
@@ -97,9 +97,9 @@ export class AnomalyComponent implements OnInit {
             cancelButtonText: 'いいえ'
         }).then((result) => {
             if (result.value) {
-            this.loading = true;
-                this.anomalyService.deleteAnomaly(row.id).subscribe((res)=>{
-                    if (res){
+                this.loading = true;
+                this.anomalyService.deleteAnomaly(row.id).subscribe((res) => {
+                    if (res) {
                         Swal.fire(
                             '削除!',
                             'タイムラインが削除されました。.',
@@ -112,10 +112,10 @@ export class AnomalyComponent implements OnInit {
         })
     }
 
-    editAnomaly(row){
-        const dialogRef = this.dialog.open(AddAnomalyComponent, {data: row, width: '440px'});
+    editAnomaly(row) {
+        const dialogRef = this.dialog.open(AddAnomalyComponent, { data: row, width: '440px' });
         dialogRef.afterClosed().subscribe(res => {
-            if (res){
+            if (res) {
                 this.getAllAnomalys();
                 let message = "編集されたタイムライン";
                 this._snackBar.open(message, 'OK', {
@@ -126,10 +126,10 @@ export class AnomalyComponent implements OnInit {
 
     }
 
-    addAnomaly(){
-        const dialogRef = this.dialog.open(AddAnomalyComponent, {width: '440px'});
+    addAnomaly() {
+        const dialogRef = this.dialog.open(AddAnomalyComponent, { width: '440px' });
         dialogRef.afterClosed().subscribe(res => {
-            if (res){
+            if (res) {
                 this.getAllAnomalys();
                 let message = "新しいタイムラインが追加されました";
                 this._snackBar.open(message, 'OK', {
@@ -140,8 +140,12 @@ export class AnomalyComponent implements OnInit {
 
     }
 
-    goToReport(row){
-        this.router.navigate([row.id + "/report"], {relativeTo: this.route});
+    showMap(row){
+        console.log(row);        
+    }
+
+    goToReport(row) {
+        this.router.navigate([row.id + "/report"], { relativeTo: this.route });
     }
 
     handlePage(e: any) {
