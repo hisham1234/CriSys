80b82f4155c106e559ef2cbdbc0dbe1ea20314be
Senaneuch Lucien Bertrand <senaneuch.lucien@enexcosmh.onmicrosoft.com>
2021-06-30 08:37:47 +0000

    Updated environment.test.ts modify url to match with the test environment service

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
   //   pathToAPI :'http://13.78.69.173:5000'
-    pathToAPI :'https://crisys-ui.azurewebsites.net' 
+    pathToAPI :'https://anomaly-service.azurewebsites.net/' 
   };
