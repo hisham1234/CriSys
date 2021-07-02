301cd37c42cef9d935e226f16bdd76371928ec9d
Senaneuch Lucien Bertrand <senaneuch.lucien@enexcosmh.onmicrosoft.com>
2021-07-02 00:26:24 +0000

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
 //   pathToAPI :'http://13.78.69.173:5000'
-  pathToAPI :'http://localhost:5000'
+  pathToAPI :'https://anomaly-service-prod.azurewebsites.net'
 
 };
