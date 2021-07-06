377d79d4dea401bbe7bce3839989313f83d6dd6a
Sathsara <s.manamperi@enexcosmh.onmicrosoft.com>
2021-07-06 13:31:05 +0900

    added LoginComponent path

--- src/app/components/main-routing.module.ts
+++ src/app/components/main-routing.module.ts
@@ -4,24 +4,29 @@ import { AnomalyComponent } from './anomaly/anomaly.component';
 import { ReportListComponent } from './report-list/report-list.component';
 import { LayoutComponent } from '../admin/layout/layout.component';
 import { childRoutes } from './child-routes';
+import { LoginComponent } from '../login/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    children: [
-            ...childRoutes
-    ]
-  }
-//   ,
-//   {
-//     path: 'anomaly/:id/event',
-//     component: EventListComponent,
-//   }
+    children: [...childRoutes],
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+
+  //   ,
+  //   {
+  //     path: 'anomaly/:id/event',
+  //     component: EventListComponent,
+  //   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MainRoutingModule { }
+export class MainRoutingModule {}
