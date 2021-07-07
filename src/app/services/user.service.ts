3d1fc33583683e94e69f9895c72f04e862d7bf4f
Sathsara <s.manamperi@enexcosmh.onmicrosoft.com>
2021-07-07 12:03:05 +0900

    user services

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,46 +1,18 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
-import {UserModel} from '../models/user.model';
-import {environment} from '../../environments/environment';
-import {HttpUtilsService} from "./http-utils.service";
-import {Router} from "@angular/router";
-import {QueryParamsModel} from "../models/query-param.model";
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-const USER_URL = `${environment.pathToAPI}/api/v1/users`;
+import { User } from '../models/user';
+import { environment } from 'src/environments/environment';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class UserService {
-    constructor(
-        private http: HttpClient,
-        private httpUtils: HttpUtilsService,
-        private router: Router
-    ) {}
+  constructor(private http: HttpClient) {}
 
+  getAll() {  
+    return this.http.get<User[]>(`${environment.apiUrl}/users`);
+  }
 
-    getUsers(queryParams:QueryParamsModel) {
-    	console.log('UserService.getUsers!');
-        const headers = this.httpUtils.getHTTPHeaders();
-        return this.http.post(USER_URL+'/find-filtered',queryParams, { headers: headers });
-    }
-
-    deleteEmployee(user_id:number) {
-    	console.log('UserService.deleteUser!');
-        const headers = this.httpUtils.getHTTPHeaders();
-        return this.http.delete(USER_URL+'/'+user_id, { headers: headers });
-    }
-
-    editEmployee(user_id:number, user:UserModel) {
-    	console.log('UserService.editUser!');
-        const headers = this.httpUtils.getHTTPHeaders();
-        return this.http.put(USER_URL+'/'+user_id, user,{ headers: headers });
-    }
-
-    createEmployee(user:UserModel) {
-    	console.log('UserService.createUser!');
-        const headers = this.httpUtils.getHTTPHeaders();
-        return this.http.post(USER_URL, user,{ headers: headers });
-    }
-
-
+  getById(id: number) {
+    return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
+  }
 }
