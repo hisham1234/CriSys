b3005b240b04035169eb86d46c7c2bc87df37e73
Sathsara <s.manamperi@enexcosmh.onmicrosoft.com>
2021-07-07 11:54:33 +0900

    aded a temporary "sign out" button to top-nav bar

--- src/app/admin/layout/top-nav/top-nav.component.ts
+++ src/app/admin/layout/top-nav/top-nav.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-top-nav',
   templateUrl: './top-nav.component.html',
-  styleUrls: ['./top-nav.component.scss']
+  styleUrls: ['./top-nav.component.scss'],
 })
 export class TopNavComponent implements OnInit {
+  user: User;
   @Output() sideNavToggled = new EventEmitter<void>();
 
-  constructor(private readonly router: Router) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private readonly router: Router
+  ) {
+    this.authenticationService.user.subscribe((x) => (this.user = x));
+  }
 
   ngOnInit() {}
 
@@ -17,8 +25,12 @@ export class TopNavComponent implements OnInit {
     this.sideNavToggled.emit();
   }
 
-  goToHomePage(){
+  goToHomePage() {
     this.router.navigate(['/']);
+  }
+
+  logout() {
+    this.authenticationService.logout();
   }
 
   onLoggedout() {
