0dea9394d2c9f2a02330d2a2bca64412ce75ac5b
Sathsara <s.manamperi@enexcosmh.onmicrosoft.com>
2021-07-08 10:08:48 +0900

    Conditioned the authorization with environment var

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,24 +1,39 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate,Router, RouterStateSnapshot } from "@angular/router";
-import { AuthenticationService } from "../services/authentication.service";
+import { Injectable } from '@angular/core';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { ThemeService } from 'ng2-charts';
+import { environment } from 'src/environments/environment';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-    constructor(
-        private router: Router,
-        private authenticationService: AuthenticationService
-    ) { }
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        const user = this.authenticationService.userValue;
-        if (user) {         
-            if (route.data.roles && route.data.roles.indexOf(user.role) === -1) {         
-                this.router.navigate(['/']);
-                return false;
-            }
-            return true;
-        }
-        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
-        return false;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    
+    // It should return 'true' for development environment
+    //so it is not needed to log in when developing. 
+    if (!environment.production) {
+      return true;
     }
+
+    const user = this.authenticationService.userValue;
+    if (user) {
+      if (route.data.roles && route.data.roles.indexOf(user.role) === -1) {
+        this.router.navigate(['/']);
+        return false;
+      }
+      return true;
+    }
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
 }
