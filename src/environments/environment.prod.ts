4a17860b2eb23e9de372b42a228d29abc96674fc
NAWZAR MOHAMEDHISHAM MOHAMED <m.nawzarmohamedhisham@enexcosmh.onmicrosoft.com>
2021-07-13 17:57:39 +0900

    changedServicePath

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   baseUrl: 'http://localhost:4200',
   production: true,
   //   pathToAPI :'http://13.78.69.173:5000'
-  pathToAPI: 'https://anomaly-service-prod.azurewebsites.net',
+  pathToAPI: 'https://anomaly-service.azurewebsites.net',
   apiUrl: 'http://localhost:4200',
 };
