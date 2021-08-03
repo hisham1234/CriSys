49d40099aab425502b99aef252daddecba74d69c
Narthana Sathsara Manamperi <s.manamperi@enexcosmh.onmicrosoft.com>
2021-08-03 16:39:01 +0900

    created dropdown menu

--- src/app/components/add-anomaly/add-anomaly.component.ts
+++ src/app/components/add-anomaly/add-anomaly.component.ts
@@ -4,14 +4,32 @@ import {ErrorStateMatcher} from "@angular/material/core";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {AnomalyModel} from "../../models/anomaly.model";
 import {AnomalyService} from "../../services/anomaly.service";
-
+export interface DistasterType {
+  value: string;
+  viewValue: string;
+  icon : string
+}
 @Component({
   selector: 'app-add-anomaly',
   templateUrl: './add-anomaly.component.html',
   styleUrls: ['./add-anomaly.component.scss']
 })
 export class AddAnomalyComponent implements OnInit {
-
+  selectedValue: string;
+  selectTypeText= $localize `Anomaly Type`;
+  distasterTypes: DistasterType[] = [
+    {value: '自然災害', viewValue: '自然災害', icon :"disasters"},
+    {value: '地震', viewValue: '地震', icon :"earthquake"},
+    {value: '事故等', viewValue: '事故等', icon :"accident"},
+    {value: '異常降雨', viewValue: '異常降雨', icon :"rain"},
+    {value: '強風', viewValue: '強風', icon :"wind"},
+    {value: '豪雪', viewValue: '豪雪', icon :"snowfall"},
+    {value: '火山災害', viewValue: '火山災害', icon :"volcanic"},
+    {value: '原子力災害', viewValue: '原子力災害', icon :"nuclear"},
+    {value: 'テロ等', viewValue: 'テロ等', icon :"terrorism"},
+    {value: 'その他', viewValue: 'その他', icon :"others"},
+    {value: '会計検査', viewValue: '会計検査', icon :"audit"},
+  ];
   anomalyForm: FormGroup;
   title: any='異常を追加'; //Add anomaly
   errorMessage=$localize `Please Fill Up the details`; //Please fill up the form
