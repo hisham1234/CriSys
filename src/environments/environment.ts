06376e44e49fb0010e978f2797c5829424e2e30f
senaneuch.lucien <senaneuch.lucien@enexcosmh.onmicrosoft.com>
2021-08-12 09:22:16 +0900

    [Auto-Refresh] Auto-refresh list of reports and anomaly

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
   production: true,
   //   pathToAPI :'http://13.78.69.173:5000'
-  pathToAPI: 'http://localhost:5001',
- // pathToAPI: 'https://anomaly-service.azurewebsites.net',
+  //pathToAPI: 'http://localhost:5001',
+  pathToAPI: 'https://anomaly-service.azurewebsites.net',
   apiUrl: 'http://localhost:4200',
-  arcGisUrl:'https://nee-bousai1.maps.arcgis.com/apps/dashboards/c90637c801b64db2b49dc08eec84f84e#AnomalyId='
+  arcGisUrl: 'https://nee-bousai1.maps.arcgis.com/apps/dashboards/c90637c801b64db2b49dc08eec84f84e#AnomalyId='
 };
 
 /*
