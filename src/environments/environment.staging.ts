d1a16035d6e073681c8d6c1950d61948ffaf9f50
senaneuch.lucien <senaneuch.lucien@enexcosmh.onmicrosoft.com>
2021-08-18 14:02:44 +0900

    [Authentication] Update Test and Staging Env for authentication

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,8 +2,7 @@ export const environment = {
   production: true,
   //   pathToAPI :'http://13.78.69.173:5000'
   pathToAPI: 'https://anomaly-service-staging.azurewebsites.net',
-  organizationUrl: 'http://localhost:4200',
+  organizationUrl: 'https://organization-service-staging.azurewebsites.net',
   //arcGisUrl:'https://nee-bousai1.maps.arcgis.com/apps/dashboards/c90637c801b64db2b49dc08eec84f84e#AnomalyId='
   arcGisUrl:'https://nee-bousai1.maps.arcgis.com/apps/dashboards/3cbf58c5c9a44dba84aa6162e405d309#AnomalyId='
-  
 };
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   //   pathToAPI :'http://13.78.69.173:5000'
   pathToAPI: 'https://anomaly-service.azurewebsites.net',
-  organizationUrl: 'http://localhost:4200',
+  organizationUrl: 'https://organization-service.azurewebsites.net',
   arcGisUrl:'https://nee-bousai1.maps.arcgis.com/apps/dashboards/c90637c801b64db2b49dc08eec84f84e#AnomalyId='
 };
