cd858b881f0273429467f7ca863a9c757d8f8c74
senaneuch.lucien <senaneuch.lucien@enexcosmh.onmicrosoft.com>
2021-08-23 10:44:48 +0900

    [Display Settings] Add translation

--- src/app/components/display-settings/display-settings.component.ts
+++ src/app/components/display-settings/display-settings.component.ts
@@ -3,30 +3,28 @@ import { UserModel } from 'src/app/models/user.model';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
-  selector: 'app-display-settings',
-  templateUrl: './display-settings.component.html',
-  styleUrls: ['./display-settings.component.css']
+    selector: 'app-display-settings',
+    templateUrl: './display-settings.component.html',
+    styleUrls: ['./display-settings.component.css']
 })
 export class DisplaySettingsComponent implements OnInit {
 
-  constructor(private authentication: AuthenticationService) { }
+    constructor(private authentication: AuthenticationService) { }
 
-  user: UserModel;
-  refreshRate: number;
-  ngOnInit(): void {
-      this.authentication.userSubject.subscribe((user) => {
-        this.user = user;
-        this.refreshRate = user.refreshRate;
-      });
-      this.authentication.emitUser();
-  }
+    user: UserModel;
+
+    ngOnInit(): void {
+        this.authentication.userSubject.subscribe((user) => {
+            this.user = user;
+        });
+        this.authentication.emitUser();
+    }
 
 
-  save(){
-      //this.user.refreshRate = this.refreshRate;
-      this.authentication.updateUserByToken(this.user).subscribe((user) => {
-        this.authentication.updateUser(user);  
-      });
+    save() {
+        this.authentication.updateUserByToken(this.user).subscribe((user) => {
+            this.authentication.updateUser(user);
+        });
     }
 
 }
